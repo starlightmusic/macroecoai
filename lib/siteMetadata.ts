77ba260759c38365e0a6c857228871da335a73be
Deepak <ddk220.ai@gmail.com>
2025-06-15 15:51:29 -0700

    Revert "Remove unused icons and add env example"
    This reverts commit c81e566802d288dba22197382bd0773118395723.

--- lib/siteMetadata.ts
+++
@@ -1,6 +0,0 @@
-export const siteMetadata = {
-  twitter: 'https://twitter.com/example',
-  linkedin: 'https://www.linkedin.com/company/example',
-  github: 'https://github.com/example',
-};
-export type SiteMetadata = typeof siteMetadata;
